bbc8ffa0be048a49121e1d180d2d0594b114a458
wr4thf.dyu <wr4thf.dyu@gmail.com>
2026-09-02 23:20:39 +0300

    Update Logger and Add Qt base

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <csignal>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 #include "utils/KernelLogger/KernelLogger.h"
 
-int main(){
+
+
+int main(int argc, char *argv[]){
     KernelLogger logger("CVault");
+    static KernelLogger* glLogger = &logger;
     logger.info("Initializing CryptoVault");
-    return 0;
+
+    logger.info("Starting Qt");
+    QGuiApplication app(argc, argv);
+    std::signal(SIGINT, [](int signum) {
+        glLogger->warn("SIGINT CRASH");
+    });
+    logger.info("Starting Qt/QML engine");
+    QQmlApplicationEngine engine;
+
+    logger.info("Starting GUI");
+    return app.exec();
 }
